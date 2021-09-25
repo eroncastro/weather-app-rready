82f537d225624667193dfe827d90188a0b5a1e3b
Eron Castro <eroncastro@gmail.com>
2021-09-25 14:06:32 -0300

    Fix entrypoint

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      shrink-on-scroll
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Weather App</v-toolbar-title>
+    </v-app-bar>
+
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {}
+
+</script>
+
+<style lang="css">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
