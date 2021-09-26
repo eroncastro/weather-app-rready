b4cdebd4858792106fb10f1de3f78647e38397d1
Eron Castro <eroncastro@gmail.com>
2021-09-26 13:46:37 -0300

    Add vue router note

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,9 @@ Vue.use(VueRouter);
 
 const Settings = { template: '<div>Settings</div>' };
 
+// https://router.vuejs.org/guide/essentials/passing-props.html
+// passing props to avoid coupling
+
 const routes = [
   { path: '/', component: CityList },
   { path: '/settings', component: Settings },
