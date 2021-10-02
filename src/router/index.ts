b98e79cbd545144907e1edf65fb109afc194ecb2
Eron Castro <eroncastro@gmail.com>
2021-10-01 23:56:55 -0300

    Add Setting view

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import CityList from '../components/CityList.vue';
+import Settings from '../components/Settings.vue';
 
 Vue.use(VueRouter);
-
-const Settings = { template: '<div>Settings</div>' };
 
 // https://router.vuejs.org/guide/essentials/passing-props.html
 // passing props to avoid coupling
@@ -21,7 +20,10 @@ const routes = [
       openWeatherMapApiClient
     },
   },
-  { path: '/settings', component: Settings },
+  {
+    path: '/settings',
+    component: Settings
+  },
 ];
 
 export default new VueRouter({
